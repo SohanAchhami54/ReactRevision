5444761417728bc3203c2c2a40b6ea95d6abfb46
Sohan Ac <sohanachhami655@gmail.com>
2025-09-22 17:31:53 +0545

    understand the folder structure

---
+++ JSX/src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from './Hello'
+
+const App = () => {
+  return (
+    <>
+    <h1 className=' text-xl font-bold flex justify-center'>My name is Sohan Achhami.</h1>
+    <Hello/>
+    </>
+  )
+}
+
+export default App
---
+++ JSX/src/Hello.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+   <div className='min-h-screen flex items-center justify-around'>
+   <div>1</div>
+   <div>2</div>
+   <div>3</div>
+   <div>4</div>
+   <div>5</div>
+   </div>
+  )
+}
+
+export default Hello

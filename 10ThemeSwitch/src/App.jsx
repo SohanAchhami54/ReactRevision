7185add45f7ac3a94afe7fc29620be8c2c4cb209
Sohan Ac <sohanachhami655@gmail.com>
2025-10-24 13:36:23 +0545

    implemented dark/light theme toggle with Context Api

---
+++ 10ThemeSwitch/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ThemeProvider } from './context/theme'
+import { Card, ThemeBtn } from './components'
+
+function App() {
+const [themeMode,setThemeMode]=useState("light")
+
+const darkTheme=()=>{
+  setThemeMode("dark")
+}
+const lightTheme=()=>{
+  setThemeMode("light")
+}
+
+useEffect(()=>{
+ document.querySelector('html').classList.remove("light","dark")
+ document.querySelector('html').classList.add(themeMode)
+},[themeMode])
+
+  return (
+    <>
+      <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+        <h1 className='text-2xl bg-amber-200'>Hello my name is sohan achhami</h1>
+         <ThemeBtn />
+          <Card />
+       
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
---
+++ 10ThemeSwitch/src/components/ThemeBtn.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useTheme } from '../context/theme'
+
+const ThemeBtn = () => {
+    const {themeMode,darkTheme,lightTheme} =useTheme()
+    
+    const handleTheme=(e)=>{
+        const darkModeStatus=e.currentTarget.checked
+        if(darkModeStatus){
+            darkTheme()
+        }else{
+            lightTheme()
+        }
+
+    }
+
+  return (
+    <div className='flex gap-2'>
+      <input type="checkbox"
+      value=''
+      checked={themeMode==="dark"}
+      onChange={handleTheme}
+      />
+      <h1>Toggle Theme</h1>
+    </div>
+  )
+}
+
+export default ThemeBtn

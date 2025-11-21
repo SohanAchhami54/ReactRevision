21456621a7a5b57e540a74ce91a95e079e797bd8
Sohan Ac <sohanachhami655@gmail.com>
2025-11-21 11:36:57 +0545

    optimize password generator

--- PasswordGenerator06/src/App.jsx
+++ PasswordGenerator06/src/App.jsx
@@ -1,64 +1,57 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from 'react'
+
 const App = () => {
   const [count,setCount]=useState(8);
-  const [password,setPassword]=useState("");
-  const [isNumber,setIsNumber]=useState(false);
-  const [isCharacter,setIsCharacter]=useState(false);
- 
-  //useRef
-  const passwordRef=useRef(null);
+  const [isNumber,setIsNumber]=useState(false)
+  const [isCharacter,setIsCharacter]=useState(false)
+  const [password,setPassword]=useState('');
 
- const passwordGenerator=useCallback(()=>{
-   let result="";
-   let string="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   if(isNumber) string+="0123456789";
-   if(isCharacter) string+="@#$%^&*(){}[]|";
+  const passwordGenerator=useCallback(()=>{
+    let str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let passgenerated=''
+    if (isNumber) str+='0123456789'
+     if (isCharacter) str+='!@#$%^&*()+{}[],.||<>'
+
+    for(let i=0;i<count;i++){
+        let index=Math.floor(Math.random()*str.length);
+        passgenerated+=str[index];
+    }
+     setPassword(passgenerated)
+  },[count,isNumber,isCharacter]) 
    
-   for(let i=0;i<count;i++){
-    let randomLength=Math.floor(Math.random()*string.length);
-    result+=string[randomLength];
-   }
-   setPassword(result);
+   useEffect(()=>{
+    passwordGenerator();
+   },[count,isNumber,isCharacter]);
 
- },[count,isCharacter,isNumber]);
-
- useEffect(()=>{
-  passwordGenerator();
- },[count,isNumber,isCharacter])
-
-
- const copyToClipBoard=()=>{
-    passwordRef.current?.select();
-    window.navigator.clipboard.writeText(password);
- }
 
   return (
     <>
-      <div className='text-2xl flex flex-col justify-center items-center min-h-screen bg-gray-800'>
-       
-        <div className="bg-gray-600 p-10 rounded">
-        <h1>Password Generator</h1>
-      <div className='flex gap-2 mt-4'>
-        <input type="text" className='border rounded-lg w-100' value={password} ref={passwordRef} />
-        <button onClick={copyToClipBoard}>Copy</button>
-      </div>
-      <div className="flex gap-2 justify-center items-center">
-        length:{count}
-        <input type="range" min={8} max={16} value={count}
-        onChange={(e)=>setCount(e.target.value)}
-        />
+    <div className='bg-amber-600 min-h-screen flex flex-col justify-center items-center'>
+         <h1>My name is sohan achhami.</h1>
+         <div className='bg-blue-400/20 px-4 py-4 flex  justify-center items-center gap-4 mb-6 md:mb-10'>
+          <input type="text" value={password}
+          className='border border-white rounded-xl px-10 py-2' />
+          <button>
+            copy
+          </button>
+         </div>
+         <div className='flex gap-4'>
+          <div className='flex gap-2'>
+               <h2>length:{count} </h2>
+            <input type="range" min={8} max={15} value={count}
+             onChange={(e)=>setCount(e.target.value)}
+            />
+          </div>
+          <div className='flex gap-3'>
+            <label htmlFor="number">Number</label>
+           <input type="checkbox" id='number' onClick={()=>setIsNumber((prev)=>!prev)} />
 
-        <label htmlFor="number">Number</label>
-        <input type="checkbox" id="number" onClick={()=>setIsNumber((prev)=>!prev)} />
-
-
-        <label htmlFor="character">Character</label>
-        <input type="checkbox" id="character" onClick={()=>setIsCharacter((prev)=>!prev)} />
-      </div>
-
-
-      </div>
-      </div>
+            <label htmlFor="string">String</label>
+           <input type="checkbox" id='string'  onClick={()=>setIsCharacter((prev)=>!prev)}  />
+          </div>
+             
+         </div>
+    </div>
     </>
   )
 }

85b57d2af82b7f6d439d683764f0aa133e740e50
Sohan Ac <sohanachhami655@gmail.com>
2026-05-05 17:09:47 +0545

    perf(passgenerator): improve performance by usecallback

--- PasswordGenerator06/src/App.jsx
+++ PasswordGenerator06/src/App.jsx
@@ -1,59 +1,170 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, {useCallback, useEffect, useRef, useState } from 'react'
 
 const App = () => {
-  const [count,setCount]=useState(8);
-  const [isNumber,setIsNumber]=useState(false)
-  const [isCharacter,setIsCharacter]=useState(false)
-  const [password,setPassword]=useState('');
-
+  const [length,setLength]=useState(8)
+  const [password,setPassword]=useState('')
+  const [number,setNumber]=useState(false) 
+  const [string,setString]=useState(false)
+  
+  const passwordRef=useRef(null)
+  
+ 
   const passwordGenerator=useCallback(()=>{
-    let str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    let passgenerated=''
-    if (isNumber) str+='0123456789'
-    if (isCharacter) str+='!@#$%^&*()+{}[],.||<>'
+   let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+   let pass=""
+   if(number) str+="0123456789"
+   if(string) str+="~!@#$%^&*()_+=[]{};:'"
 
-    for(let i=0;i<count;i++){
-        let index=Math.floor(Math.random()*str.length);
-        passgenerated+=str[index];
-    }
-     setPassword(passgenerated)
-  },[count,isNumber,isCharacter]) 
-   
-   useEffect(()=>{
-    passwordGenerator();
-   },[count,isNumber,isCharacter]);
+   for(let i=1;i<=length;i++){
+      let index=Math.floor(Math.random()*str.length+1) 
+      pass+=str.charAt(index)
+   }
+    setPassword(pass)
+  },[length,number,string])
 
+  
+  const copypasswordToClipboard=()=>{
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,15)
+  }
+
+
+  useEffect(()=>{
+      passwordGenerator()
+  },[length,number,string])
 
   return (
     <>
-    <div className='bg-amber-600 min-h-screen flex flex-col justify-center items-center'>
-         <h1>My name is sohan achhami.</h1>
-         <div className='bg-blue-400/20 px-4 py-4 flex  justify-center items-center gap-4 mb-6 md:mb-10'>
-          <input type="text" value={password}
-          className='border border-white rounded-xl px-10 py-2' />
-          <button>
-            copy
-          </button>
-         </div>
-         <div className='flex gap-4'>
+    <div className='mx-auto max-w-md m-5 p-4 rounded-2xl'>
+          <h1>Password Generator</h1>
           <div className='flex gap-2'>
-               <h2>length:{count} </h2>
-            <input type="range" min={8} max={15} value={count}
-             onChange={(e)=>setCount(e.target.value)}
-            />
+            <input type="text" value={password} readOnly
+            ref={passwordRef} className='border rounded-lg w-full' />
+            <button onClick={copypasswordToClipboard} 
+            className='p-1 bg-green-400 rounded-lg'>Copy</button>
           </div>
-          <div className='flex gap-3'>
-            <label htmlFor="number">Number</label>
-           <input type="checkbox" id='number' onClick={()=>setIsNumber((prev)=>!prev)} />
+          <div className='flex gap-2 mt-2'>
+            <input type="range" min={1} max={30} onChange={(e)=>setLength(e.target.value)} /> 
+            <span>length:{length} </span>
 
-            <label htmlFor="string">String</label>
-           <input type="checkbox" id='string'  onClick={()=>setIsCharacter((prev)=>!prev)}  />
+            <label htmlFor="number">Number:</label>
+            <input type="checkbox" id='number' onClick={()=>setNumber(prev=>!prev)} />
+
+            <label htmlFor="string">String:</label>
+            <input type="checkbox" id='string' onClick={()=>setString(prev=>!prev)} />
           </div>
-             
-         </div>
+         
     </div>
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useCallback, useEffect, useState } from 'react'
+
+// const App = () => {
+//   const [count,setCount]=useState(8);
+//   const [isNumber,setIsNumber]=useState(false)
+//   const [isCharacter,setIsCharacter]=useState(false)
+//   const [password,setPassword]=useState('');
+
+//   const passwordGenerator=useCallback(()=>{
+//     let str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//     let passgenerated=''
+//     if (isNumber) str+='0123456789'
+//     if (isCharacter) str+='!@#$%^&*()+{}[],.||<>'
+
+//     for(let i=0;i<count;i++){
+//         let index=Math.floor(Math.random()*str.length);
+//         passgenerated+=str[index];
+//     }
+//      setPassword(passgenerated)
+//   },[count,isNumber,isCharacter]) 
+   
+//    useEffect(()=>{
+//     passwordGenerator();
+//    },[count,isNumber,isCharacter]);
+
+
+//   return (
+//     <>
+//     <div className='bg-amber-600 min-h-screen flex flex-col justify-center items-center'>
+//          <h1>My name is sohan achhami.</h1>
+//          <div className='bg-blue-400/20 px-4 py-4 flex  justify-center items-center gap-4 mb-6 md:mb-10'>
+//           <input type="text" value={password}
+//           className='border border-white rounded-xl px-10 py-2' />
+//           <button>
+//             copy
+//           </button>
+//          </div>
+//          <div className='flex gap-4'>
+//           <div className='flex gap-2'>
+//                <h2>length:{count} </h2>
+//             <input type="range" min={8} max={15} value={count}
+//              onChange={(e)=>setCount(e.target.value)}
+//             />
+//           </div>
+//           <div className='flex gap-3'>
+//             <label htmlFor="number">Number</label>
+//            <input type="checkbox" id='number' onClick={()=>setIsNumber((prev)=>!prev)} />
+
+//             <label htmlFor="string">String</label>
+//            <input type="checkbox" id='string'  onClick={()=>setIsCharacter((prev)=>!prev)}  />
+//           </div>
+             
+//          </div>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default App
